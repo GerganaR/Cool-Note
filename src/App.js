d99f2755e80066ef1a36d8552cafd6ed1e345336
unknown <www.g.roshleva7@gmail.com>
2022-01-19 21:06:27 +0200

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
+import react, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const clearText = () => {
+    setText("");
+  };
+
+  const saveText = () => {
+    localStorage.setItem("note", JSON.stringify(text));
+    setText("");
+  };
+
+  // useEffect(() => {
+  //   const data = localStorage.getItem("note", text);
+  //   setText(data);
+  // }, [text]);
+
+  window.addEventListener("load", function () {
+    const data = localStorage.getItem("note", text);
+    setText(data);
+    console.log(data);
+  });
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea
+              onChange={(e) => setText(e.target.value)}
+              className="textarea is-large"
+              placeholder="Notes..."
+              value={text}
+            />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button
+          onClick={saveText}
+          className="button is-large is-primary is-active"
+        >
+          Save
+        </button>
+        <button onClick={clearText} className="button is-large">
+          Clear
+        </button>
       </div>
     </div>
   );
